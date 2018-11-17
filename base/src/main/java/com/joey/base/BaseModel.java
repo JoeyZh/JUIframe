3a3db3fa7fbb48112f31d948c5a912a6f921feb2
Joeyzh <lma_ma@163.com>
2018-11-17 13:36:55 +0800

    增加baseModel的方法

--- base/src/main/java/com/joey/base/BaseModel.java
+++ base/src/main/java/com/joey/base/BaseModel.java
@@ -1,10 +1,22 @@
 package com.joey.base;
 
 import java.io.Serializable;
+import java.lang.reflect.Method;
+import java.lang.reflect.ParameterizedType;
 
 /**
  * Created by Joey on 2018/2/24.
  */
 
 public abstract class BaseModel implements Serializable {
+
+    public Object getValue(String key) throws Exception{
+
+        Method[] m = getClass().getMethods();
+        for (int i = 0; i < m.length; i++) {
+            if (("get" + key).toLowerCase().equals(m[i].getName().toLowerCase()))
+                return m[i].invoke(this);
+        }
+        return null;
+    }
 }
